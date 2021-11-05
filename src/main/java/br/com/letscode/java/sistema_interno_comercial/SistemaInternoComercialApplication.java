3120c750e90c6ddbe2c76780568bf3b825bd56ba
Roger <rogerio.alonso@soulasalle.com.br>
2021-11-05 13:31:54 -0300

    Create Project Spring

---
+++ src/main/java/br/com/letscode/java/sistema_interno_comercial/SistemaInternoComercialApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.java.sistema_interno_comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInternoComercialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaInternoComercialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/letscode/java/sistema_interno_comercial/SistemaInternoComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.java.sistema_interno_comercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInternoComercialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
