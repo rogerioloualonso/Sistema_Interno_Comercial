b95fda99b3a440b2b85dbe4c2ee7452a85873644
luizjoana <luizinho177@hotmail.com>
2021-11-09 17:03:16 -0300

    classe venda

--- src/main/java/br/com/letscode/java/sistemacomercial/entity/venda/Venda.java
+++ src/main/java/br/com/letscode/java/sistemacomercial/entity/venda/Venda.java
@@ -1,4 +1,32 @@
 package br.com.letscode.java.sistemacomercial.entity.venda;
 
+import br.com.letscode.java.sistemacomercial.entity.endereco.Endereco;
+import br.com.letscode.java.sistemacomercial.entity.loja.Loja;
+import br.com.letscode.java.sistemacomercial.entity.produto.Produto;
+import lombok.Data;
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+
 public class Venda {
+
+    Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private LocalDate data;
+    private String endereco;
+
+    @OneToMany(mappedBy = "id")
+    private Venda item;
+
+    @OneToMany(mappedBy = "id")
+    private Produto funcionarios;
+
+
 }
