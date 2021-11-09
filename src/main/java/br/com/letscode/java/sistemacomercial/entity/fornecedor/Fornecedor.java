f1f1c2cbd5725387c111db7586385408ae35c077
Mateus Rodrigues de Govêa <matrgovea@gmail.com>
2021-11-09 15:53:11 -0300

    Funcionário e Fornecedor

--- src/main/java/br/com/letscode/java/sistemacomercial/entity/fornecedor/Fornecedor.java
+++ src/main/java/br/com/letscode/java/sistemacomercial/entity/fornecedor/Fornecedor.java
@@ -1,19 +1,29 @@
 package br.com.letscode.java.sistemacomercial.entity.fornecedor;
 
+import br.com.letscode.java.sistemacomercial.entity.endereco.Endereco;
 import lombok.Data;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
 public class Fornecedor {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+    private int telefone;
 
+    @OneToMany(mappedBy = "id")
+    private List<Endereco> endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/br/com/letscode/java/sistemacomercial/entity/funcionario/Funcionario.java
+++ src/main/java/br/com/letscode/java/sistemacomercial/entity/funcionario/Funcionario.java
@@ -1,4 +1,29 @@
 package br.com.letscode.java.sistemacomercial.entity.funcionario;
 
+import br.com.letscode.java.sistemacomercial.entity.endereco.Endereco;
+import br.com.letscode.java.sistemacomercial.entity.venda.Venda;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
 public class Funcionario {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+
+        private String nome;
+        private LocalDate dataNascimento;
+        private int cpf;
+        private int telefone;
+
+        @OneToMany(mappedBy = "id")
+        private Venda venda;
+
+        @OneToMany(mappedBy = "id")
+        private List<Endereco> endereço;
 }
