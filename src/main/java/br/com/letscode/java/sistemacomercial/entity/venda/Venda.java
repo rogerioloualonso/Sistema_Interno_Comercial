67691c749cc5b04552b923076cc38b5174cb569b
Roger <rogerio.alonso@soulasalle.com.br>
2021-11-09 17:18:31 -0300

    Consertando erro classe vendas

--- src/main/java/br/com/letscode/java/sistemacomercial/entity/venda/Venda.java
+++ src/main/java/br/com/letscode/java/sistemacomercial/entity/venda/Venda.java
@@ -1,21 +1,18 @@
 package br.com.letscode.java.sistemacomercial.entity.venda;
 
-import br.com.letscode.java.sistemacomercial.entity.endereco.Endereco;
-import br.com.letscode.java.sistemacomercial.entity.loja.Loja;
-import br.com.letscode.java.sistemacomercial.entity.produto.Produto;
+import br.com.letscode.java.sistemacomercial.entity.funcionario.Funcionario;
+import br.com.letscode.java.sistemacomercial.entity.item.Item;
 import lombok.Data;
-import org.apache.tomcat.jni.Local;
-
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@Entity
-@Data
 
+@Data
+@Entity
 public class Venda {
 
-    Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
@@ -23,10 +20,9 @@ public class Venda {
     private String endereco;
 
     @OneToMany(mappedBy = "id")
-    private Venda item;
+    private List<Item> item;
 
     @OneToMany(mappedBy = "id")
-    private Produto funcionarios;
-
+    private List<Funcionario> funcionarios;
 
 }
