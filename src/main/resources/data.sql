73ec4ab2d556990e45a6cb3343a7a1d8820f7e97
Roger <rogerio.alonso@soulasalle.com.br>
2021-11-10 20:12:42 -0300

    add swagger

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
+/*insert into funcionario (id, nome, cpf) values (1, "Manoel Abreu", 123456);*/
 
-//insert into funcionario(nome, cpf) values("Manoel Abreu", 123456);
+/*insert into produto (id, nome, qtdEstoque, valor) values (1, "pão", 100, 1);*/
