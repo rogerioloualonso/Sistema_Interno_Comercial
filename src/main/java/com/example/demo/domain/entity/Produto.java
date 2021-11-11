83d4fc8bbc4e6965d170f0a8c0ba1640a055e0e0
Marcus Feitosa <marcusfeitosa02@gmail.com>
2021-11-11 08:45:11 -0300

    Cadastro de produto

--- src/main/java/com/example/demo/domain/entity/Produto.java
+++ src/main/java/com/example/demo/domain/entity/Produto.java
@@ -1,12 +1,13 @@
 package com.example.demo.domain.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.List;
-
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
---
+++ src/main/java/com/example/demo/rest/ProdutoController.java
@@ -0,0 +1,34 @@
+package com.example.demo.rest;
+
+import com.example.demo.domain.entity.Produto;
+import com.example.demo.domain.repository.Produtos;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+
+public class ProdutoController {
+    private Produtos produtos;
+    public ProdutoController(Produtos produtos){
+        this.produtos=produtos;
+    }
+    @GetMapping("{id}")
+    public Produto getClienteById( @PathVariable Integer id ){
+        return produtos
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Cliente não encontrado"));
+}
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto save( @RequestBody Produto produto ){
+        return produtos.save(produto);
+    }
+}
