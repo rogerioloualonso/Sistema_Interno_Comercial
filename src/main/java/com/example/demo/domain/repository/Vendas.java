f68711e64e4948ee87dd8646e0ccef17005ab0d1
Mateus Rodrigues de Govêa <matrgovea@gmail.com>
2021-11-11 16:04:42 -0300

    Mais Correções

--- src/main/java/com/example/demo/domain/repository/Vendas.java
+++ src/main/java/com/example/demo/domain/repository/Vendas.java
@@ -8,10 +8,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface Vendas extends JpaRepository<Venda, Integer>{
-    @Query(value = " select * from venda f where f.id like '%:id%' ", nativeQuery = true)
+    @Query(value = " select * from vendas f where f.id like '%:id%' ", nativeQuery = true)
     List<Venda> encontrarPorId(@Param("id") int id );
 
-    @Query(" delete from venda f where f.id =:id ")
+    @Query(" delete from vendas f where f.id =:id ")
     @Modifying
     void deleteById(int id);
 
