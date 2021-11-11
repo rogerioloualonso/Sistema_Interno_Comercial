0b397205fd834ae5f0034cd792cf872599ba47b3
Mateus Rodrigues de Govêa <matrgovea@gmail.com>
2021-11-11 15:38:52 -0300

    Adicionando Funcionalidades das Vendas, pt.2

--- src/main/java/com/example/demo/domain/repository/Vendas.java
+++ src/main/java/com/example/demo/domain/repository/Vendas.java
@@ -1,6 +1,20 @@
 package com.example.demo.domain.repository;
 
+import com.example.demo.domain.entity.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface Vendas extends JpaRepository<com.example.demo.domain.entity.Vendas, Integer>{
+    @Query(value = " select * from vendas f where f.id like '%:id%' ", nativeQuery = true)
+    List<Funcionario> encontrarPorId(@Param("id") int id );
+
+    @Query(" delete from Vendas f where f.id =:id ")
+    @Modifying
+    void deleteById(int id);
+
+    boolean existsById(int id);
 }
--- src/main/java/com/example/demo/rest/VendasController.java
+++ src/main/java/com/example/demo/rest/VendasController.java
@@ -2,12 +2,16 @@ package com.example.demo.rest;
 
 import com.example.demo.domain.entity.Funcionario;
 import com.example.demo.domain.entity.Vendas;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/vendas")
+@RequestMapping("/vendas")
 public class VendasController {
     private Vendas vendas;
     public VendasController(Vendas vendas) {
@@ -39,6 +43,33 @@ public class VendasController {
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Venda não encontrada") );
-
     }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update( @PathVariable Integer id,
+                        @RequestBody Vendas vendas){
+        vendas
+                .findById(id)
+                .map(vendaExistente -> {
+                    vendas.setId(vendaExistente.getId());
+                    vendas.save(vendas);
+                    return vendaExistente;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Venda não encontrada") );
+    }
+
+    @GetMapping
+    public List<Funcionario> find(Funcionario filtro ){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(
+                        ExampleMatcher.StringMatcher.CONTAINING );
+
+        Example example = Example.of(filtro, matcher);
+        return funcionarios.findAll(example);
+    }
+
+
 }
